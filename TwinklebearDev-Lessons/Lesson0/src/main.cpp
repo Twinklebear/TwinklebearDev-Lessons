c9bd9cc5b67bf6d49880284c71839f63f1a29177
Will Usher <willusher.life@gmail.com>
2012-07-11 21:35:18 -0400

    Committing lesson 0

---
+++ TwinklebearDev-Lessons/Lesson0/src/main.cpp
@@ -0,0 +1,11 @@
+#include "SDL.h"
+
+/*
+*	Lesson0: A simple program to make sure your SDL environment is set up correctly
+*/
+int main(int argc, char** argv){
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Quit();
+	
+	return 0;
+}
