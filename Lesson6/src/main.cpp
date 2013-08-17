3d320186c6661567e25845f295735f8a09b79970
Twinklebear <willusher.life@gmail.com>
2013-08-16 19:06:09 -0700

    Updating mac makefile & removing comments

--- Lesson6/src/main.cpp
+++ Lesson6/src/main.cpp
@@ -9,12 +9,10 @@
 #elif defined(__clang__)
 #include <SDL2/SDL.h>
 #include <SDL2_image/SDL_image.h>
-//A guess at where the file will be, TODO: check
 #include <SDL2_ttf/SDL_ttf.h>
 #else
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-//A guess at where the file will be, TODO: check
 #include <SDL2/SDL_ttf.h>
 #endif
 
