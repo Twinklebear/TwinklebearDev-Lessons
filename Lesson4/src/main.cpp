0bd883602ee8e28111ade76fc82d42e86b27b137
Will <willusher.life@gmail.com>
2014-02-28 23:12:54 -0800

    Migrating Lesson 4 & 5

--- Lesson4/src/main.cpp
+++ Lesson4/src/main.cpp
@@ -1,54 +1,46 @@
 #include <string>
 #include <iostream>
-
-#if defined(_MSC_VER)
 #include <SDL.h>
 #include <SDL_image.h>
-#elif defined(__clang__)
-#include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-#else
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#endif
+#include "asset.h"
 
 /*
-* Lesson 4: Handling Events
-*/
+ * Lesson 4: Handling Events
+ */
 //Screen attributes
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 
-/**
-* Log an SDL error with some error message to the output stream of our choice
-* @param os The output stream to write the message too
-* @param msg The error message to write, format will be msg error: SDL_GetError()
-*/
+/*
+ * Log an SDL error with some error message to the output stream of our choice
+ * @param os The output stream to write the message too
+ * @param msg The error message to write, format will be msg error: SDL_GetError()
+ */
 void logSDLError(std::ostream &os, const std::string &msg){
 	os << msg << " error: " << SDL_GetError() << std::endl;
 }
-/**
-* Loads an image into a texture on the rendering device
-* @param file The image file to load
-* @param ren The renderer to load the texture onto
-* @return the loaded texture, or nullptr if something went wrong.
-*/
+/*
+ * Loads an image into a texture on the rendering device
+ * @param file The image file to load
+ * @param ren The renderer to load the texture onto
+ * @return the loaded texture, or nullptr if something went wrong.
+ */
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
 	SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
 	if (texture == nullptr)		
 		logSDLError(std::cout, "LoadTexture");
 	return texture;
 }
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
-* width and height
-* @param tex The source texture we want to draw
-* @param rend The renderer we want to draw too
-* @param x The x coordinate to draw too
-* @param y The y coordinate to draw too
-* @param w The width of the texture to draw
-* @param h The height of the texture to draw
-*/
+/*
+ * Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
+ * width and height
+ * @param tex The source texture we want to draw
+ * @param rend The renderer we want to draw too
+ * @param x The x coordinate to draw too
+ * @param y The y coordinate to draw too
+ * @param w The width of the texture to draw
+ * @param h The height of the texture to draw
+ */
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h){
 	//Setup the destination rectangle to be at the position we want
 	SDL_Rect dst;
@@ -58,14 +50,14 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int
 	dst.h = h;
 	SDL_RenderCopy(ren, tex, NULL, &dst);
 }
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-* the texture's width and height
-* @param tex The source texture we want to draw
-* @param rend The renderer we want to draw too
-* @param x The x coordinate to draw too
-* @param y The y coordinate to draw too
-*/
+/*
+ * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+ * the texture's width and height
+ * @param tex The source texture we want to draw
+ * @param rend The renderer we want to draw too
+ * @param x The x coordinate to draw too
+ * @param y The y coordinate to draw too
+ */
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
 	int w, h;
 	SDL_QueryTexture(tex, NULL, NULL, &w, &h);
@@ -82,7 +74,7 @@ int main(int argc, char** argv){
 	//Setup our window and renderer, this time let's put our window in the center 
 	//of the screen
 	SDL_Window *window = SDL_CreateWindow("Lesson 4", SDL_WINDOWPOS_CENTERED, 
-		SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+			SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == nullptr){
 		logSDLError(std::cout, "CreateWindow");
 		return 2;
@@ -92,9 +84,9 @@ int main(int argc, char** argv){
 		logSDLError(std::cout, "CreateRenderer");
 		return 3;
 	}
-	
+
 	//The texture we'll be using
-	SDL_Texture *image = loadTexture("../res/Lesson4/image.png", renderer);
+	SDL_Texture *image = loadTexture(ASSET("Lesson4/image.png"), renderer);
 	if (image == nullptr)
 		return 4;
 
@@ -137,6 +129,6 @@ int main(int argc, char** argv){
 
 	IMG_Quit();
 	SDL_Quit();
-	
+
 	return 0;
 }
