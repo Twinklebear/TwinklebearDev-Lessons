bcbb31fbd4fc3c35cba626dc139f8ba5da0c0601
Will <willusher.life@gmail.com>
2014-08-01 14:01:09 -0700

    Do actual template specialization

--- include/cleanup.h
+++ include/cleanup.h
@@ -4,28 +4,32 @@
 #include <SDL.h>
 
 /*
- * These serve to free the passed argument and also provide the
- * base cases for the template recursion
- */
-void cleanup(SDL_Window *win){
-	SDL_DestroyWindow(win);
-}
-void cleanup(SDL_Renderer *ren){
-	SDL_DestroyRenderer(ren);
-}
-void cleanup(SDL_Texture *tex){
-	SDL_DestroyTexture(tex);
-}
-void cleanup(SDL_Surface *surf){
-	SDL_FreeSurface(surf);
-}
-/*
  * Clean up each argument in the list
  */
 template<typename T, typename... Args>
 void cleanup(T *t, Args&&... args){
 	cleanup(t);
 	cleanup(std::forward<Args>(args)...);
+}
+/*
+ * These serve to free the passed argument and also provide the
+ * base cases for the template recursion
+ */
+template<>
+void cleanup<SDL_Window>(SDL_Window *win){
+	SDL_DestroyWindow(win);
+}
+template<>
+void cleanup<SDL_Renderer>(SDL_Renderer *ren){
+	SDL_DestroyRenderer(ren);
+}
+template<>
+void cleanup<SDL_Texture>(SDL_Texture *tex){
+	SDL_DestroyTexture(tex);
+}
+template<>
+void cleanup<SDL_Surface>(SDL_Surface *surf){
+	SDL_FreeSurface(surf);
 }
 
 #endif
