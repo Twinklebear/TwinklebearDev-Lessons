aa051df45270ea2cac2b3d7b596b6f6706c85e7c
Will <willusher.life@gmail.com>
2014-08-01 15:29:45 -0700

    Make it safe to pass nullptr through to cleanup

--- include/cleanup.h
+++ include/cleanup.h
@@ -22,18 +22,30 @@ void cleanup(T *t, Args&&... args){
  */
 template<>
 void cleanup<SDL_Window>(SDL_Window *win){
+	if (!win){
+		return;
+	}
 	SDL_DestroyWindow(win);
 }
 template<>
 void cleanup<SDL_Renderer>(SDL_Renderer *ren){
+	if (!ren){
+		return;
+	}
 	SDL_DestroyRenderer(ren);
 }
 template<>
 void cleanup<SDL_Texture>(SDL_Texture *tex){
+	if (!tex){
+		return;
+	}
 	SDL_DestroyTexture(tex);
 }
 template<>
 void cleanup<SDL_Surface>(SDL_Surface *surf){
+	if (!surf){
+		return;
+	}
 	SDL_FreeSurface(surf);
 }
 
